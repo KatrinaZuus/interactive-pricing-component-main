f9db6ca5220601d7eea598e145cbb2d579dc02d4
KatrinaZuus <katrinazuus@gmail.com>
2024-03-04 23:26:52 +0400

    toFixed

--- script.js
+++ script.js
@@ -9,32 +9,31 @@ let pageviews = document.getElementsByClassName("pageviews")[0]
 acount = 1
 function acounting() {
     if (range.value == 0) {
-        bill.innerText = `$${8.00*acount}`;
+        bill.innerText = `$${(8.00*acount).toFixed(2)}`;
         pageviews.innerText = "10K PAGEVIEWS"
         range.style.background = "linear-gradient(to right, #7aeadf 0%, #ecf0fb 0% 100%)";
-    console.log(range)
-    console.log(bill.innerText)
+    
     } 
     if (range.value == 25) {
-        bill.innerText = `$${12.00*acount}`
+        bill.innerText = `$${(12.00*acount).toFixed(2)}`
         pageviews.innerText = "50K PAGEVIEWS"
         range.style.background = "linear-gradient(to right, #7aeadf 25%, #ecf0fb 25% 100%)";
     }
 
     if (range.value == 50) {
-        bill.innerText = `$${16.00*acount}`
+        bill.innerText = `$${(16.00*acount).toFixed(2)}`
         pageviews.innerText = "100K PAGEVIEWS"
         range.style.background = "linear-gradient(to right, #7aeadf 50%, #ecf0fb 50% 100%)";
     }
 
     if (range.value == 75) {
-        bill.innerText = `$${24.00*acount}`
+        bill.innerText = `$${(24.00*acount).toFixed(2)}`
         pageviews.innerText = "500K PAGEVIEWS"
         range.style.background = "linear-gradient(to right, #7aeadf 75%, #ecf0fb 75% 100%)";
     }
 
     if (range.value == 100) {
-        bill.innerText = `$${36.00*acount}`
+        bill.innerText = `$${(36.00*acount).toFixed(2)}`
         pageviews.innerText = "1M PAGEVIEWS"
         range.style.background = "linear-gradient(to right, #7aeadf 100%, #ecf0fb 100% 100%)";
     }
